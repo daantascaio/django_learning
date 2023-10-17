ed311dd6c9a59b08717fad5915e18e7dcb1e7dc0
Caio Dantas <caiodantasl.lara@gmail.com>
2023-10-17 14:48:58 -0300

    saving

--- home/views.py
+++ home/views.py
@@ -1,10 +1,10 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def home(request):
     print('home')
+
     return render(
         request,
-        'home.html'
+        'home/index.html'
     )
