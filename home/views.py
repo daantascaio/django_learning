0898b13a9c80a7442dff5ccfa4ea6ec86f8bf96b
Caio Dantas <caiodantasl.lara@gmail.com>
2023-10-18 17:37:01 -0300

    saving

--- home/views.py
+++ home/views.py
@@ -4,7 +4,14 @@ from django.shortcuts import render
 def home(request):
     print('home')
 
+    context = {
+        'text': 'Estamos na home'
+    }
+
     return render(
         request,
-        'home/index.html'
+        'home/index.html',
+        {
+            'text': 'Estamos na home'
+        }
     )
