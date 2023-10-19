f5ec79b8bd5ccec69673d9a1735d3ea4da29e93d
Caio Dantas <caiodantasl.lara@gmail.com>
2023-10-19 15:49:40 -0300

    oauisd

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,14 @@
 
 from django.shortcuts import render
+from blog.data import posts
 
 
 def blog(request):
     print('blog')
 
     context = {
-        'text': 'Estamos no blog'
+        'text': 'Estamos no blog',
+        'posts': posts
     }
 
     return render(
