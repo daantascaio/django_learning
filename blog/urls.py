76337cb3e8e53377c17058ebcc6df5ca9d6ddc1f
Caio Dantas <caiodantasl.lara@gmail.com>
2023-10-19 17:31:32 -0300

    12ub3

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,6 @@ app_name = 'blog'
 # blog/
 urlpatterns = [
     path('', views.blog, name='blog'),
+    path('post/<id>', views.blog, name='post'),
     path('exemplo/', views.exemplo, name='exemplo'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,27 @@
-
-from django.shortcuts import render
 from blog.data import posts
+from django.shortcuts import render
 
 
 def blog(request):
     print('blog')
 
     context = {
-        'text': 'Estamos no blog',
+        # 'text': 'Olá blog',
+        'posts': posts
+    }
+
+    return render(
+        request,
+        'blog/index.html',
+        context
+    )
+
+
+def post(request, id):
+    print('post', id)
+
+    context = {
+        # 'text': 'Olá blog',
         'posts': posts
     }
 
@@ -22,7 +36,8 @@ def exemplo(request):
     print('exemplo')
 
     context = {
-        'text': 'Estamos no exemplo'
+        'text': 'Olá exemplo',
+        'title': 'Essa é uma página de exemplo - ',
     }
 
     return render(
