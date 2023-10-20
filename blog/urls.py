84197f113fb6e5a34bbfde139599d3f52014ee68
Caio Dantas <caiodantasl.lara@gmail.com>
2023-10-20 08:53:17 -0300

    URLs

--- blog/urls.py
+++ blog/urls.py
@@ -4,8 +4,10 @@ from django.urls import path
 app_name = 'blog'
 
 # blog/
+# Django URLs:
+# https://docs.djangoproject.com/en/4.2/topics/http/urls/
 urlpatterns = [
-    path('', views.blog, name='blog'),
+    path('', views.blog, name='home'),
     path('post/<id>', views.blog, name='post'),
     path('exemplo/', views.exemplo, name='exemplo'),
 ]
