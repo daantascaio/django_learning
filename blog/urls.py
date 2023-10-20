a1bad9a710a11c3edac288b87df75bd48333ff3b
Caio Dantas <caiodantasl.lara@gmail.com>
2023-10-20 09:03:07 -0300

    ubqae

--- blog/urls.py
+++ blog/urls.py
@@ -8,6 +8,6 @@ app_name = 'blog'
 # https://docs.djangoproject.com/en/4.2/topics/http/urls/
 urlpatterns = [
     path('', views.blog, name='home'),
-    path('post/<id>', views.blog, name='post'),
+    path('<int:id>/', views.post, name='post'),
     path('exemplo/', views.exemplo, name='exemplo'),
 ]
